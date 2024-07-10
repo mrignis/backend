b8d469753d1bf2f7f9f6c0d34674577712b8ad2b
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-07-10 13:20:06 -0400

    Update registerUser.js

--- models/registerUser.js
+++ models/registerUser.js
@@ -16,3 +16,4 @@ registerSchema.methods.comparePassword = async function(candidatePassword) {
 const registerUser = mongoose.model('registerUser', registerSchema);
 
 export default registerUser;
+r
